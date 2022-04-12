d37956d2f4c1a0dec58c9c7c4256f0293ff81c56
Augustine Mutua <augustinemutua.am@gmail.com>
2022-04-12 03:01:39 +0300

    Update app.py

--- exercises/002-sum_of_three_numbers/app.py
+++ exercises/002-sum_of_three_numbers/app.py
@@ -1,7 +1,10 @@
 # Sum all three input numbers and print on the console the result
-first_number = input("First input")
-second_number = input("Second input")
-third_number = input("Third input")
+
+a = int(input("First input"))
+b = int (input("Second input"))
+c= int (input("Third input"))
 # print here the sum of three inputs
 
-print(first_number+second_number)
+x=a+b+c
+
+print("The Sum is",x)
