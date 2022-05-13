c5321599ae50b5d3013788df9df62fab92eac28e
Augustine Mutua <augustinemutua.am@gmail.com>
2022-05-13 03:15:28 +0300

    Update app.py

--- exercises/003-area_of_right_triangle/app.py
+++ exercises/003-area_of_right_triangle/app.py
@@ -3,8 +3,8 @@ b = int (input("Size of B"))
 h = int(input("Size of H"))
 
 def area_of_triangle(arg1, arg2):
-    area_of_triangle =0.5*b*h 
-    return area_of_triangle
+    
+    return 0.5*float(arg1)*float(arg2)
 
 # Testing your function
 print(area_of_triangle(b, h)) 
